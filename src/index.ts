88c002e00064ab6af76bc3204a847474d60c4e91
Inonhe Henrique (SO/OPM74-BR) <Henrique.Inonhe@br.bosch.com>
2020-11-13 10:58:45 -0300

    Mostly complete

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { promises as fs } from "fs";
-import { stringify } from "querystring";
-import { Components, HashMap, Info, OpenAPI, Operation, Parameter, Path, Reference, RequestBody, Schema, Tag, isReference, isArraySchema, isEnumerableSchema, isObjectSchema, Responses, Response } from "./models";
+import { Components, HashMap, Info, OpenAPI, Operation, Parameter, Path, Reference, RequestBody, Response, Responses, Schema, Tag, isArraySchema, isEnumerableSchema, isObjectSchema, isReference } from "./models";
+
 import { capitalize } from "./utils";
+import { promises as fs } from "fs";
+import { type } from "os";
 
 interface OperationEntry {
   pathString : string;
@@ -22,7 +23,7 @@ async function main() : Promise<void> {
   
   await writeApiClients(openApiObject);
   await writeReadme(openApiObject.info);
-  // await writeModels(openApiObject);
+  await writeModels(openApiObject);
 }
 
 main();
@@ -50,7 +51,8 @@ async function writeApiClients(openApiObject : OpenAPI) : Promise<void> {
 
 async function writeSingleApiClient(name : string, description : string | undefined, tag : string, operationGroup : OperationGroup, baseUrl : string, components : Components) : Promise<void> {
     let apiClientText = "";
-    apiClientText +=            `import axios from "axios";\n`;
+    apiClientText +=            `import axios, { AxiosResponse } from "axios";\n`;
+    apiClientText +=            `import * as Models from "./models";\n`;
     apiClientText +=            `\n`;
     apiClientText +=            `/**\n`;
     apiClientText +=            ` * ${name}\n`;
@@ -110,7 +112,7 @@ function buildApiClientMethodString(operation : Operation, verb : string, url : 
     methodText +=        ` *\n`;
   }
   methodText +=          ` */\n`;
-  methodText +=          `  public static async ${operationId}(${buildApiClientMethodArgumentString(operation, components)}) : Promise<${buildApiClientMethodReturnTypeString(responses, components)}> { \n`;
+  methodText +=          `  public static async ${operationId}(${buildApiClientMethodArgumentString(operation, components)}) : Promise<AxiosResponse<${buildApiClientMethodReturnTypeString(responses, components)}>> { \n`;
   methodText +=          `    const response = await axios({\n`;
   methodText +=          `      method: "${verb}",\n`;
   methodText +=          `      url: \`${url}\`,\n`;
@@ -123,7 +125,7 @@ function buildApiClientMethodString(operation : Operation, verb : string, url : 
   }
   methodText +=          `      }\n`;
   methodText +=          `    });\n`;
-  methodText +=          `    return await response.data;\n`;
+  methodText +=          `    return response;\n`;
   methodText +=          `  }\n`;
   methodText +=          `\n`;
 
@@ -142,23 +144,22 @@ function buildApiClientMethodArgumentString(operation : Operation, components : 
   if(requestBody) {
     const resolvedRequestBody = resolve(components, requestBody);
     const required = resolvedRequestBody.required || false;
-    const schema = resolvedRequestBody.content["application/json"].schema; //Hard coded but might change in the future
-    const resolvedSchema = resolve(components, schema) as Schema;
+    const schema = resolvedRequestBody.content["application/json"].schema!; //Hard coded but might change in the future
 
     if(required) {
-      requiredArguments.push(`body : ${resolveType(resolvedSchema)}`);
+      requiredArguments.push(`body : ${buildTypeString(components, schema)}`);
     }
     else {
-      optionalArguments.push(`body ?: ${resolveType(resolvedSchema)}`);
+      optionalArguments.push(`body ?: ${buildTypeString(components, schema)}`);
     }
   }
 
   if(parameters) {
     for(const parameter of parameters) {
       const resolvedParameter = resolve(components, parameter);
+      const schema = resolvedParameter.schema!;
       const parameterName = resolvedParameter.name;
-      const resolvedSchema = resolve(components, resolvedParameter.schema) as Schema;
-      const parameterType = resolveType(resolvedSchema);
+      const parameterType = buildTypeString(components, schema);
       const parameterIn = resolvedParameter.in;
       const required = parameterIn === "path" ? true : (resolvedParameter.required || false);
       if(required) {
@@ -188,12 +189,11 @@ function buildApiClientMethodReturnTypeString(responses : Responses | Reference<
   for(const httpResponse in actualResponses) {
     const response = (actualResponses as Record<string, Response | Reference<Response>>)[httpResponse];
     const actualResponse = resolve(components, response);
-    const schema = actualResponse.content!["application/json"].schema;
-    const actualSchema = resolve(components, schema)!;
-    schemas.push(actualSchema);
+    const schema = actualResponse.content!["application/json"].schema!;
+    schemas.push(schema);
   }
 
-  return schemas.map(schema => resolveType(schema as Schema)).join(" | ");
+  return schemas.map(schema => buildTypeString(components, schema)).join(" | ");
 }
 
 async function writeReadme(info : Info) : Promise<void> {
@@ -245,28 +245,27 @@ async function writeReadme(info : Info) : Promise<void> {
   await fs.writeFile("./api/README.md", readmeText);
 }
 
-// async function writeModels(openApiObject : OpenAPI) : Promise<void> {
-//   const components = openApiObject.components;
+async function writeModels(openApiObject : OpenAPI) : Promise<void> {
+  const components = openApiObject.components;
 
-//   let modelsText = "";
-//   if(components) {
-//     if(components.schemas) {
-//       for(const key in components.schemas) {
-//         const schema = components.schemas[key]!;
-//         const resolvedSchema = isReference(schema) ? resolveReference(components, schema.$ref) : schema;
+  let modelsText = "";
+  if(components) {
+    if(components.schemas) {
+      for(const key in components.schemas) {
+        const schema = components.schemas[key]!;
+        const resolvedSchema = resolve(components, schema);
 
-//         if(resolvedSchema.type === "object") {
-//           modelsText +=        `export interface ${capitalize(key)}Schema {\n`;
-//           modelsText +=        `${resolveInterface(schema)}`;
-//           modelsText +=        `}\n`;
-//           modelsText +=        `\n`;
-//         }
-//       }
-//     }
-//   }
+        if(resolvedSchema.type === "object") {
+          modelsText +=        `export interface ${capitalize(key)} `;
+          modelsText +=        `${buildTypeString(components, schema, false)}`;
+          modelsText +=        `;\n\n`;
+        }
+      }
+    }
+  }
 
-//   await fs.writeFile("./api/models.ts", modelsText);
-// }
+  await fs.writeFile("./api/models.ts", modelsText);
+}
 
 function groupOperationsByTag(paths : HashMap<Path>) : OperationGroupTable {
   const verbs = ["get", "put", "post", "delete", "options", "head", "patch", "trace"];
@@ -309,52 +308,69 @@ function resolveReferenceExplicitely<T>(components : Components, reference : Ref
   return currentObject;
 }
 
-function resolveType(schema : Schema) : string {
-  const { type } = schema;
+function resolveReferenceImplicitely<T>(reference : Reference<T>) : string {
+  const refPath = reference.$ref;
+  const breadcrumbs = refPath.split("/").slice(2);
+  return breadcrumbs[breadcrumbs.length - 1];
+}
+
+function buildTypeString(components : Components, schema : Schema | Reference<Schema>, inline = true) : string {
+  if(isReference(schema)) {
+    return buildImplicitTypeString(schema);
+  }
+
+  return buildExplicitTypeString(components, schema, inline);
   
-  if(isEnumerableSchema(schema)) { //integer, number and string types
-    const enumeration = schema.enum;
+}
+
+function buildImplicitTypeString(schema : Reference<Schema>) : string{
+  return `Models.${resolveReferenceImplicitely(schema)}`;
+}
+
+function buildExplicitTypeString(components : Components, schema : Schema | Reference<Schema>, inline : boolean) : string {
+  const resolvedSchema = resolve(components, schema);
+  const { type } = resolvedSchema;
+  
+  if(isEnumerableSchema(resolvedSchema)) { //integer, number and string types
+    const enumeration = resolvedSchema.enum;
     if(enumeration) {
       return enumeration.join(" | ");
     }
+  }
+
+  if(isArraySchema(resolvedSchema)) {
+    const itemsSchema = resolve(components, resolvedSchema.items);
+    return `Array<${buildTypeString(components, itemsSchema)}>`;    
+  }
+
+  if(isObjectSchema(resolvedSchema)) {
+    let typeString = `{${inline ? " " : "\n"}`;
     
-    if(type === "integer") {
-      return "number";
-    }
-
-    return type;
-  }
-
-  if(isArraySchema(schema)) {
-    const itemsSchema = schema.items;
-    return `Array<${resolveType(itemsSchema)}>`;    
-  }
-
-  if(isObjectSchema(schema)) {
-    let typeString = `{ `;
-
-    const properties = schema.properties;
+    const properties = resolvedSchema.properties;
     for(const key in properties) {
-      const property = properties[key];
-      typeString += `${key} : ${resolveType(property)}; `;
+      const propertySchema = resolve(components, properties[key]);
+      const required = propertySchema.required || false;
+      const typeJudgementSymbol = required ? ":" : "?:";
+      typeString += `${inline ? "" : "  "}${key} ${typeJudgementSymbol} ${buildTypeString(components, propertySchema)};${inline ? " " : "\n"}`;
     }
-    typeString += ` }`;
+    typeString += `}`;
     return typeString;
   }
 
-  return "boolean";
+  if(type === "integer") {
+    return `number`;
+  }
+
+  if(type === "boolean" ||
+     type === "number" ||
+     type === "string") {
+    return `${type}`;
+  }
+
+  throw new Error(`"${type}" is not a valid type!`);
+  //Maybe we should handle this with more care
+  //And also deal with null type
 }
-
-// function resolveInterface(schema : Schema) : string {
-//   let interfaceText = "";
-//   const properties = schema.properties;
-//   for(const key in properties) {
-//     const property = properties[key];
-//     interfaceText += `  ${key} : ${resolveType(property.type)};\n`;
-//   }
-
-//   return interfaceText;
-// }
 
 function resolve<T>(components : Components, object : T | Reference<T>) : T {
   if(isReference(object)) {
